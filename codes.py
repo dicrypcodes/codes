6ca881aec223b52e089555bec60fc3f258ebaac2
dicrypcodes~ <daisidaniel2004@gmail.com>
2023-08-26 18:04:21 +0100

    main setup

---
+++ codes.py
@@ -0,0 +1,135 @@
+k = 0
+while k < 6:
+  k +=1
+  if k==4:
+    continue
+  print(k)  
+  
+
+
+lifestyle = "what kind of lifestyle is this?"
+movement = "he has being denjeloux lately!"
+for x in movement:
+  print(x)
+  if x == "i" or x== "a":
+    break
+
+print("it is less than it!") if len(lifestyle)< len(movement) else print("what a life!!") 
+  
+    
+print(type(lifestyle))
+print(type(movement))
+
+I = 0
+while I < 6:
+  print(I)
+  I +=1
+else:
+  print("I is no longer less than 6") 
+
+
+a = 200
+b = 33
+c = 500
+if a > b or c > a:
+  print("one of the  conditions is True")
+else:
+  print("not working")  
+
+d = 49
+e = 50
+if not e > d:
+  print("e is greater than d!")
+else:
+  print("d is less than e!")
+ 
+    
+
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
+i = 1
+while i < 6:
+  print(i)    
+  if i == 4:
+    break
+  i +=1
+
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x== "banana":
+    continue
+
+
+num = 0,1, 2, 3, 4, 5
+for i in num:
+  if i < 5:
+    i +=1
+    if i==4:
+      continue
+    print(i)
+
+for x in range(1,11):
+  if x == 7:
+    break
+  print(x)
+
+for x in range(2, 30, 3):
+  print(x)  
+else:
+  print("finally finished!!")  
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+food = ["rice", "egg", "beans", "eba"]
+
+for x in adj:
+  for y in fruits:
+    for z in food:
+      print(x,y,z)
+     
+
+
+
+
+schools =["oau", "ui", "uniben", "unilag", "lasu"]
+for x in schools:
+    print(x)
+    if x == "uniben":
+      break
+   
+y = "content of life"
+def my_life():
+  global y
+  y =  "it has being a rollarcostal!"
+my_life()  
+print(y)
+
+
+  
+  
+  
+
+
+
+ 
+    
+    
+    
+
+    
+
+
+
+
+
+  
