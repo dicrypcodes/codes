d4e8de02f7a642e6ae8b62ffb9ce1a86264871d7
dicrypcodes~ <daisidaniel2004@gmail.com>
2023-09-04 23:40:29 +0100

    Array in python

---
+++ Array.py
@@ -0,0 +1,12 @@
+cars = ["golf", "benz", "bmw", "mazda"]
+cars[0] = "gwagon"
+for x in cars:
+    if "a" in x:
+        print(x)
+
+name = ["Daniel", "Seyi", "Yoga", "Barbie"]
+name.append("Gbenga")
+name.pop(1)
+name.remove("Daniel")
+print(name)
+
