a56c566415a9a9cd7d39aaadbc3ec627f2d5f299
dicrypcodes~ <daisidaniel2004@gmail.com>
2023-11-25 19:47:31 +0100

    restart.py

---
+++ restart.py
@@ -0,0 +1,15 @@
+#first lineo of code after restarting python from scratch
+print("Hello, world!")
+
+#python identation
+if 8>2:
+     print("Eight is greater than two!")#the amount of space for indention is left for the programmer to decide
+     print("Eight")#you have to use the same space of indentation for the same block of code
+
+#python variables
+#variables are created when a value is assigned to it. the equals to sign is use to assign value
+x = 5
+y = "Hello world!"
+print(x)
+print(y)
+
