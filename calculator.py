3a3f726aa870d313026b5bb8cc6816f8d72f3a48
dicrypcodes~ <daisidaniel2004@gmail.com>
2023-12-22 23:08:56 +0100

    updating calculator

--- calculator.py
+++ calculator.py
@@ -12,25 +12,28 @@ def calculator():
     print("2. sub")
     print("3. multiply")
     print("4. divide")
-    choice = input("(enter a number from 1 to 4): ")
+    choice = (input("(enter a number from 1 to 4): "))
     num1= int(input("enter the first number: " ))
     num2= int(input("enter the second number: "))
-    if choice == "1":
+    if choice == "1" :
         result = add(num1,num2)
-        print("answer", result)
+        print("Answer: ", result)
     if choice == "2" :
         result = sub(num1, num2)
-        print("answer", result)
+        print("Answer: ", result)
     if choice == "3" :
         result = multiply(num1, num2)
-        print("answer", result)
+        print(("Answer: ", result))
     if choice == "4" :
         result= divide(num1,num2)
-        print("answer", result)
+        print("Answer: ", result)
     else:
-        return "not valid"
+        return "ERROR!!"
     
 calculator()    
+
+#check and make answer return interger instead of floats
+#examine this calculator tomorrow
    
     
     
